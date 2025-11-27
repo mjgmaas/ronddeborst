7c1c57bf8dc68bfd12c534eb42f31889767d59fa
Maurice Maas <mjgmaas@gmail.com>
2025-11-27 14:52:45 +0100

    Add meta tags for cache control and robots, and include favicon SVG

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
     @vite(['resources/css/app.css', 'resources/css/custom-styles.css', 'resources/js/app.js'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
     <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <meta name="robots" content="noindex, nofollow" />
+
     <meta name="theme-color" content="#111827">
     @stack('head')
     <!-- Google Fonts: Sansita & Lato -->
